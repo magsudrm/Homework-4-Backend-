62084a9f967a65d364f3f4ad8034c833aab91f2e
unknown <magsudrm@code.edu.az>
2023-04-04 17:15:48 +0400

    b

---
+++ Classwork2/Eterna/Controllers/TeamController.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using Eterna.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eterna.Controllers
+{
+	public class TeamController : Controller
+	{
+		private readonly DataContext _context;
+		public TeamController (DataContext context)
+		{
+			_context = context;
+		}
+		public IActionResult Index()
+		{
+			var data = _context.TeamMembers.ToList();
+			return View(data);
+		}
+	}
+}
---
+++ Classwork2/Eterna/Models/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Eterna.Models
+{
+	public class DataContext:DbContext
+	{
+		public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+		public DbSet<TeamMember> TeamMembers { get; set; }
+	}
+}
